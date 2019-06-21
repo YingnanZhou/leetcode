c95e9121f7998203b561b7737d1f7fde87e18082
YingnanZhou <48658870+YingnanZhou@users.noreply.github.com>
2019-06-21 22:30:26 +0800

    Add files via upload

---
+++ Leetcode101.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSame(root.left, root.right);
+    }
+    
+    private boolean isSame(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        
+        return isSame(p.left, q.right) && isSame(p.right, q.left);
+    }
+}
